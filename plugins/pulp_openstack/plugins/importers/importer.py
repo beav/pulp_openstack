d78cc204823291c3409bf88f7ef24426ba226ce8
Chris Duryee <cduryee@redhat.com>
2014-06-11 15:52:28 -0400

    wip

--- plugins/pulp_openstack/plugins/importers/importer.py
+++ plugins/pulp_openstack/plugins/importers/importer.py
@@ -51,7 +51,8 @@ class OpenstackImageImporter(Importer):
         Upload a openstack image.
         See super(self.__class__, self).upload_unit() for the docblock explaining this method.
         """
-        image = models.OpenstackImage(unit_key['image_checksum'], unit_key['image_size'])
+        image = models.OpenstackImage(unit_key['image_checksum'], unit_key['image_size'],
+                                      unit_key['image_filename'])
         # not sure if this is the best way to handle init_unit, pulp_docker is a bit different
         image.init_unit(conduit)
         shutil.move(file_path, image.storage_path)
